5a7cf06badd9687b3a18287a676f968396ad9cf3
Claire Q <claireq620@gmail.com>
2023-01-24 15:58:24 -0500

    Updated stying

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import DrumMachine from './components/DrumMachine';
 
 function App() {
 	return (
-		<div className="App bg-red-900 h-screen flex justify-center items-center">
+		<div className="App bg-red-900 min-h-[1000px] min-w-[1000px] h-screen flex justify-center items-center">
 			<DrumMachine />
 		</div>
 	);
