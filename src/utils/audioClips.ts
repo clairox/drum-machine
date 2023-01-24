951d7c076aa021d60a064d1a69c4c2cd7e6703ca
Claire Q <claireq620@gmail.com>
2023-01-24 15:06:08 -0500

    Changed audio directory

--- src/utils/audioClips.ts
+++ src/utils/audioClips.ts
@@ -1,18 +1,28 @@
+import heater1 from '../assets/audio/Heater-1.mp3';
+import heater2 from '../assets/audio/Heater-2.mp3';
+import heater3 from '../assets/audio/Heater-3.mp3';
+import heater4 from '../assets/audio/Heater-4.mp3';
+import clap from '../assets/audio/Clap.mp3';
+import opnHH from '../assets/audio/OpenHH.mp3';
+import kickHat from '../assets/audio/KickHat.mp3';
+import kick from '../assets/audio/Kick.mp3';
+import closedHH from '../assets/audio/ClosedHH.mp3';
+
 export type AudioClip = {
 	id: string;
 	src: string;
 };
 
-const dir = '/audio';
+const dir = '../assets/audio';
 
 export const audioClips: AudioClip[] = [
-	{ id: 'Heater-1', src: dir + '/Heater-1.mp3' },
-	{ id: 'Heater-2', src: dir + '/Heater-2.mp3' },
-	{ id: 'Heater-3', src: dir + '/Heater-3.mp3' },
-	{ id: 'Heater-4', src: dir + '/Heater-4.mp3' },
-	{ id: 'Clap', src: dir + '/Clap.mp3' },
-	{ id: 'OpenHH', src: dir + '/OpenHH.mp3' },
-	{ id: 'KickHat', src: dir + '/KickHat.mp3' },
-	{ id: 'Kick', src: dir + '/Kick.mp3' },
-	{ id: 'ClosedHH', src: dir + '/ClosedHH.mp3' },
+	{ id: 'Heater-1', src: heater1 },
+	{ id: 'Heater-2', src: heater2 },
+	{ id: 'Heater-3', src: heater3 },
+	{ id: 'Heater-4', src: heater4 },
+	{ id: 'Clap', src: clap },
+	{ id: 'OpenHH', src: opnHH },
+	{ id: 'KickHat', src: kickHat },
+	{ id: 'Kick', src: kick },
+	{ id: 'ClosedHH', src: closedHH },
 ];
